b4c8ceb0924b8ae87791f62ec79028122b3f4075
James Baker <5226213+JayboBaker@users.noreply.github.com>
2023-03-02 11:56:53 +0000

    Strip startercode

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.scss'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1 data-cy="title" className="title is-1">
+          Todo App
+        </h1>
       </header>
     </div>
   )
